120952a8cc69d531571f88e8741ff9892be1d274
Blackilykat <github@blackilykat.dev>
2025-02-24 19:11:36 +0100

    add user input

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,8 @@ const gameTilesY: i16 = 20;
 const gameSpacingX: i16 = 20;
 const gameSpacingY: i16 = 20;
 
+const targetTickTime: f32 = 0.1;
+
 const groundColorA: Color = .{
     .r = 100,
     .g = 100,
@@ -40,13 +42,17 @@ const Position = struct { x: i32, y: i32 };
 
 pub fn main() !void {
     raylib.initWindow(windowWidth, windowHeight, windowTitle);
-    raylib.setTargetFPS(2);
+    raylib.setTargetFPS(100);
 
     var snakeHead: Position = .{ .x = gameTilesX / 2, .y = gameTilesY / 2 };
-    const snakeDirection: Direction = Direction.north;
+    var snakeDirection: Direction = Direction.north;
+
+    var currentTickTime: f32 = 0.0;
 
     while (!raylib.windowShouldClose()) {
         raylib.beginDrawing();
+
+        currentTickTime += raylib.getFrameTime();
 
         raylib.clearBackground(Color.black);
 
@@ -60,12 +66,42 @@ pub fn main() !void {
             }
         }
 
-        const newPos = torwards(snakeHead, snakeDirection);
-        if (newPos.x >= 0 and newPos.x < gameTilesX and newPos.y >= 0 and newPos.y < gameTilesY) {
-            snakeHead = newPos;
+        takeUserInput(&snakeDirection);
+
+        if (currentTickTime > targetTickTime) {
+            currentTickTime -= targetTickTime;
+            tickGame(&snakeHead, snakeDirection);
         }
 
         raylib.endDrawing();
+    }
+}
+
+fn tickGame(snakeHead: *Position, snakeDirection: Direction) void {
+    const newPos = torwards(snakeHead.*, snakeDirection);
+    if (newPos.x >= 0 and newPos.x < gameTilesX and newPos.y >= 0 and newPos.y < gameTilesY) {
+        snakeHead.* = newPos;
+    }
+}
+
+fn takeUserInput(direction: *Direction) void {
+    const Key = raylib.KeyboardKey;
+    if (raylib.isKeyDown(Key.w) or raylib.isKeyDown(Key.up)) {
+        if (direction.* != Direction.south) {
+            direction.* = Direction.north;
+        }
+    } else if (raylib.isKeyDown(Key.a) or raylib.isKeyDown(Key.left)) {
+        if (direction.* != Direction.east) {
+            direction.* = Direction.west;
+        }
+    } else if (raylib.isKeyDown(Key.s) or raylib.isKeyDown(Key.down)) {
+        if (direction.* != Direction.north) {
+            direction.* = Direction.south;
+        }
+    } else if (raylib.isKeyDown(Key.d) or raylib.isKeyDown(Key.right)) {
+        if (direction.* != Direction.west) {
+            direction.* = Direction.east;
+        }
     }
 }
 
@@ -86,4 +122,15 @@ fn torwards(position: Position, direction: Direction) Position {
         },
     }
     return newPos;
+}
+
+test "torwards" {
+    const testing = std.testing;
+
+    const pos: Position = .{ .x = 10, .y = 20 };
+
+    try testing.expectEqual(Position{ .x = 10, .y = 19 }, torwards(pos, Direction.north));
+    try testing.expectEqual(Position{ .x = 10, .y = 21 }, torwards(pos, Direction.south));
+    try testing.expectEqual(Position{ .x = 11, .y = 20 }, torwards(pos, Direction.east));
+    try testing.expectEqual(Position{ .x = 9, .y = 20 }, torwards(pos, Direction.west));
 }
